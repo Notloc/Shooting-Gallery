1e4551fe2ad30486f8b4e7484177dd801bc38e10
Notloc <ColtonJamesCampbell@gmail.com>
2019-10-04 12:58:00 -0300

    Implemented basic character controller

---
+++ Assets/Scripts/ControlBindings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Wraps the game's input names
+/// </summary>
+public static class ControlBindings
+{
+    public static string MOVE_X = "Horizontal";
+    public static string MOVE_Y = "Vertical";
+
+    public static string VIEW_X = "Mouse X";
+    public static string VIEW_Y = "Mouse Y";
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] new Rigidbody rigidbody;
     [SerializeField] new Camera camera;
 
+    [Header("Options")]
+    [SerializeField] float movementForce = 20f;
+    [Space]
+    [SerializeField] float xSensitivity = 3f;
+    [SerializeField] float ySensitivity = 3f;
+    [Space]
+    [SerializeField] float minCameraPitch = -80f;
+    [SerializeField] float maxCameraPitch = 80f;
+
+
+    private float inputX, inputY, viewX, viewY;
+    private float currentCameraRot = 0f;
     private void Update()
     {
         TakeInput();
@@ -21,16 +33,31 @@ public class PlayerController : MonoBehaviour
 
     private void TakeInput()
     {
+        inputX = Input.GetAxis(ControlBindings.MOVE_X);
+        inputY = Input.GetAxis(ControlBindings.MOVE_Y);
 
+        viewX = Input.GetAxis(ControlBindings.VIEW_X);
+        viewY = Input.GetAxis(ControlBindings.VIEW_Y);
     }
 
     private void Rotate()
     {
+        // Rotate player's body
+        this.transform.rotation *= Quaternion.Euler(0f, viewX * xSensitivity, 0f);
 
+        // Rotate camera
+        currentCameraRot -= viewY * ySensitivity;
+        currentCameraRot = Mathf.Clamp(currentCameraRot, minCameraPitch, maxCameraPitch);
+        camera.transform.localRotation = Quaternion.Euler(currentCameraRot, 0f, 0f);
     }
 
     private void Move()
     {
+        Vector3 force = new Vector3(inputX, 0f, inputY);
+        if (force.sqrMagnitude > 1f)
+            force = force.normalized;
+        force *= movementForce;
 
+        rigidbody.AddRelativeForce(force);
     }
 }
