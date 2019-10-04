6e870ca7684ff8e277f2b6b2e1cbf702aaf2ab4a
Notloc <ColtonJamesCampbell@gmail.com>
2019-10-04 12:46:42 -0300

    Created skeleton of player controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Required References")]
+    [SerializeField] new Rigidbody rigidbody;
+    [SerializeField] new Camera camera;
+
+    private void Update()
+    {
+        TakeInput();
+        Rotate();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void TakeInput()
+    {
+
+    }
+
+    private void Rotate()
+    {
+
+    }
+
+    private void Move()
+    {
+
+    }
+}
